b84cb91c632c954ad8795da1f05924b05ef4b199
davidcoles88 <davidcoles88@gmail.com>
2016-05-19 19:31:23 -0700

    Added 1.4
    Function and unit test replaces spaces with %20 for a given string.

--- CiCI 1.4/replaceSpacesWith%20.c
+++ CiCI 1.4/replaceSpacesWith%20.c
@@ -1,10 +1,8 @@
 /*Author: David Coles
 **05/18/2016
-**CtCI 1.2
+**CtCI 1.4
 **Write a method that will replace a space with %20.
  */
-
-
 
 #include<stdio.h>
 #define SIZE 100
@@ -13,39 +11,35 @@
 //Get rid of implicit declaration warning.
 int print_string(char str[], int length);
 
-int reverse_string(char str[], int length)
+int replace_space(char str[], int length)
 {
-    char reversed_str[SIZE];
+    char new_string[SIZE];
     int count = 0;
-    
+
     //Find number of vaild chars in string.
     for (int i = 0; i < length; i++)
     {
         //Ignores non-printing values.
         //32 is for white space.
-        if(str[i] > 33 && str[i] < 126 || str[i] == 32)
+        if((str[i] > 33 && str[i] < 126) || str[i] == 32)
         {
             if(str[i] == 32)
             {
-                str[i] = '%20';
+                new_string[count] = '%';
+                count++;
+                new_string[count] = '2';
+                count++;
+                new_string[count] = '0';
+                count++;
+            }
+            else{
+                new_string[count] = str[i];
+                count++;
             }
         }
     }
-    //Note count is not base on zero-based indexs, hence [count-1];
-    int temp_count = count;
-    //Reverse array
-    for (int j = 0; j < temp_count; j++)
-    {
-        //Ignores non-printing values.
-        //32 is for white space.
-        if(str[j] > 33 && str[j] < 126 || str[j] == 32)
-        {
-            reversed_str[j] = str[count-1];
-            count--;
-        }
-    }
-    print_string(reversed_str, temp_count);
-    return 1;
+    print_string(new_string, count);
+    return 0;
 }
 
 //Print string.
@@ -55,24 +49,24 @@ int print_string(char str[], int length)
     {
         printf("%c", str[i]);
     }
-    return 0;
     printf("\n");
+    return 0;
 }
 
-//Unit test for reversed_string tests one strings.
-/*int unit_test_reverse_string(){
-    printf("Unit Test: reverse_string\n");
+//Unit test for replace_space tests one strings.
+int unit_test_replace_space(){
+    printf("Unit Test: replace_space\n");
     printf("Testing: string 'abc zyx'.'\n");
     char test_string_1[SIZE] = {'a', 'b', 'c',' ', 'z','y','x'};
-    reverse_string(test_string_1, SIZE);
-}*/
+    replace_space(test_string_1, SIZE);
+}
 
 int main(int argc, char **argv)
 {
-    //unit_test_reverse_string();
+    unit_test_replace_space();
     char input_string[SIZE] = {0};
     printf("\nEnter the input string: ");
     fgets(input_string, 100, stdin);
-    reverse_string(input_string, SIZE);
+    replace_space(input_string, SIZE);
     return 0;
 }
